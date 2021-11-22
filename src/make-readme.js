f71f7340d897fa048defa98a5cf5947769d43561
Alan Taranti <alan.taranti@gmail.com>
2021-11-22 11:25:38 -0400

    atualizado make readme para lidar com multiplos arquivos

--- src/make-readme.js
+++ src/make-readme.js
@@ -2,33 +2,52 @@ const fs = require("fs");
 const path = require("path");
 
 const dados = require("./data");
-const readme = path.join(__dirname, "../README.md");
-const template = path.join(__dirname, "readme-template.md");
 
-const sumario = dados
-  .map((categoria) => `- [${categoria.titulo}](#${categoria.slug})`)
-  .join("\n");
+function gerarSumario(data) {
+  return data
+    .map((categoria) => `- [${categoria.titulo}](#${categoria.slug})`)
+    .join("\n");
+}
 
-const secoes = dados.map((categoria) => {
-  const itens = categoria.conteudo
-    .map((item) => {
+function gerarSecoes(data) {
+  return data.map((categoria) => {
+    const itens = categoria.conteudo
+      .map((item) => {
 
-      if (!item.conteudo) {
-        return `- [${item.titulo}](${item.link}) - ${item.descricao}`;
-      }
+        if (!item.conteudo) {
+          return `- [${item.titulo}](${item.link}) - ${item.descricao}`;
+        }
 
-      const subItens = item.conteudo.map(item => `  - [${item.titulo}](${item.link}) - ${item.descricao}`).join("")
+        const subItens = item.conteudo.map(item => `  - [${item.titulo}](${item.link}) - ${item.descricao}`).join("")
 
-      return `- ${item.titulo}\n${subItens}`;
-      
-    })
-    .join("");
+        return `- ${item.titulo}\n${subItens}`;
 
-  return `## ${categoria.titulo}\n\n${itens}`;
-}).join('\n\n');
+      })
+      .join("");
 
-let conteudoReadme = fs.readFileSync(template).toString();
-conteudoReadme = conteudoReadme.replace("- #PLACEHOLDER_SUMARIO#", sumario);
-conteudoReadme = conteudoReadme.replace("#PLACEHOLDER_CATEGORIAS#", secoes);
+    return `## ${categoria.titulo}\n\n${itens}`;
+  }).join('\n\n');
+}
 
-fs.writeFileSync(readme, conteudoReadme);
+function gerarArquivo(data) {
+  const metadados = data.metadados;
+  const conteudo = data.conteudo;
+  const dirpath = metadados.root ? '..' : '../modulos';
+  const filename = metadados.nomeArquivo + '.md';
+  const filepath = path.join(dirpath, `${filename}`);
+  const readme = path.join(__dirname, filepath);
+  const template = path.join(__dirname, metadados.template);
+
+  const sumario = gerarSumario(conteudo)
+
+  const secoes = gerarSecoes(conteudo);
+
+  let conteudoReadme = fs.readFileSync(template).toString();
+  conteudoReadme = conteudoReadme.replace("- #PLACEHOLDER_SUMARIO#", sumario);
+  conteudoReadme = conteudoReadme.replace("#PLACEHOLDER_CATEGORIAS#", secoes);
+
+  fs.writeFileSync(readme, conteudoReadme);
+}
+
+
+dados.forEach(dado => gerarArquivo(dado));
